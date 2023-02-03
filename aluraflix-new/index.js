fcae455e67325881d44722985a5b2bd2666149ca
Lucas Pina <ti.lucaspina@gmail.com>
2023-02-02 23:01:53 -0300

    aluraflix desafio 2 -solucao parcial

--- aluraflix-new/index.js
+++ aluraflix-new/index.js
@@ -1,34 +1,53 @@
 function adicionarFilme() {
-  let listaFilmes = [{
-    img: '',
-    nome: '',
-    trailer: ''
-  }];
+  const listaFilmes = [];
+  const listaTitulos = [];
+  const listaTrailers = [];
 
-  let filmeFavorito = document.getElementById("filme").value;
-  let nomefilme = document.getElementById("nome").value;
-  let trailerFilme = document.getElementById("trailer").value;
+  const filmeFavorito = document.getElementById("filme").value;
+  const nomefilme = document.getElementById("nome").value;
+  const trailerFilme = document.getElementById("trailer").value;
 
-  let elementoListaFilmes = document.getElementById("listaFilmes");
+  if (
+    filmeFavorito.endsWith("jpg") ||
+    filmeFavorito.endsWith("png") ||
+    (filmeFavorito.endsWith("jpeg") && filmeFavorito != "") ||
+    nomefilme != "" ||
+    trailerFilme != ""
+  ) {
+    listaFilmes.push(filmeFavorito);
+    listaTitulos.push(nomefilme);
+    listaTrailers.push(trailerFilme);
 
-  if (filmeFavorito != "" || nomefilme != "" || trailerFilme != "") {
-    listaFilmes.push({
-      img: filmeFavorito,
-      nome: nomefilme,
-      trailer: trailerFilme,
-    });
-
-    for (let index = 0; index < listaFilmes.length; index++) {
-      elementoListaFilmes.innerHTML =
-        elementoListaFilmes.innerHTML +
-        `<a target="_blank" href=${listaFilmes[i].trailer}>
-            <img src=${listaFilmes[i].img}>
-            <p>${listaFilmes[i].nome}</p>
-        </a>`;
-
-      document.getElementById("filme").value = "";
-      document.getElementById("filme").value = "";
-      document.getElementById("trailer").value = "";
-    }
+    mostrarFilmes(listaFilmes, listaTitulos, listaTrailers);
+    limparInputs();
   }
+}
+
+function mostrarFilmes(listaFilmes, listaTitulos, listaTrailers) {
+  const divFilmes = document.getElementById("listaFilmes");
+
+  for (let i = 0; i < listaFilmes.length; i++) {
+    divFilmes.innerHTML =
+      divFilmes.innerHTML +
+      `<div>
+        <a target="_blank" href=${listaTrailers[i]}>
+          <img src=${listaFilmes[i]}>
+        </a>
+        <h3>${listaTitulos[i]}</h3>
+      </div>`;
+  }
+}
+
+function limparInputs() {
+  document.getElementById("filme").value = "";
+  document.getElementById("nome").value = "";
+  document.getElementById("trailer").value = "";
+}
+
+function removerFilme() {
+  const divFilmes = document.getElementById("listaFilmes");
+
+  divFilmes.innerHTML = "";
+
+  limparInputs();
 }
