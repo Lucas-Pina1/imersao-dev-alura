a71afce14e25dd8355a84cfeef9ed23a6bdfce26
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-02-03 09:38:02 -0300

    aluraFilx desafio 2

--- aluraflix-new/index.js
+++ aluraflix-new/index.js
@@ -1,39 +1,40 @@
 function adicionarFilme() {
   const listaFilmes = [];
-  const listaTitulos = [];
-  const listaTrailers = [];
 
   const filmeFavorito = document.getElementById("filme").value;
   const nomefilme = document.getElementById("nome").value;
   const trailerFilme = document.getElementById("trailer").value;
 
+  const filme = {
+    imagem: filmeFavorito,
+    titulo: nomefilme,
+    trailer: trailerFilme,
+  };
+
   if (
     filmeFavorito.endsWith("jpg") ||
     filmeFavorito.endsWith("png") ||
-    (filmeFavorito.endsWith("jpeg") && filmeFavorito != "") ||
-    nomefilme != "" ||
-    trailerFilme != ""
+    (filmeFavorito.endsWith("jpeg") ||
+      (filmeFavorito != "" || nomefilme != "" || trailerFilme != ""))
   ) {
-    listaFilmes.push(filmeFavorito);
-    listaTitulos.push(nomefilme);
-    listaTrailers.push(trailerFilme);
+    listaFilmes.push(filme);
 
-    mostrarFilmes(listaFilmes, listaTitulos, listaTrailers);
+    mostrarFilmes(listaFilmes);
     limparInputs();
   }
 }
 
-function mostrarFilmes(listaFilmes, listaTitulos, listaTrailers) {
+function mostrarFilmes(listaFilmes) {
   const divFilmes = document.getElementById("listaFilmes");
 
   for (let i = 0; i < listaFilmes.length; i++) {
     divFilmes.innerHTML =
       divFilmes.innerHTML +
       `<div>
-        <a target="_blank" href=${listaTrailers[i]}>
-          <img src=${listaFilmes[i]}>
+        <a target="_blank" href=${listaFilmes[i].trailer}>
+          <img src=${listaFilmes[i].imagem}>
         </a>
-        <h3>${listaTitulos[i]}</h3>
+        <h3>${listaFilmes[i].titulo}</h3>
       </div>`;
   }
 }
